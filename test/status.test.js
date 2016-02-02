c54d7c80ab60e42ab98ba2630f256fbf62671c06
Darren M <aeneascorrupt@gmail.com>
2016-02-01 22:23:35 -0800

    status api

---
+++ test/status.test.js
@@ -0,0 +1,30 @@
+var expect = require('chai').expect;
+
+describe('Status Route', function () {
+    var server; 
+
+    beforeEach(function (done) {
+        var main = require('../lib/server');
+        server = main(done);
+    });
+
+    afterEach(function (done) {
+        server.stop(done);
+    });
+
+    it('returns the status', function (done) {
+        server.inject({
+            method: 'GET',
+            url: '/api/status'
+        }, function (response) {
+            var data;
+            expect(response.statusCode).to.equal(200);
+            data = JSON.parse(response.payload);
+
+            expect(data).to.deep.equal({
+                status: 'OK'
+            });
+            done();
+        });
+    });
+});
