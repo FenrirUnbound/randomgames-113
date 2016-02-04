7d09b32fa9a10b811351d3bbcf6e48b5109c6de1
Darren M <aeneascorrupt@gmail.com>
2016-02-03 23:09:44 -0800

    status functional test

--- test/functional/status.test.js
+++ test/functional/status.test.js
@@ -1,15 +1,26 @@
-var expect = require('chai').expect;
+var expect = require('chai').expect,
+    mockery = require('mockery');
 
 describe('Status Route', function () {
     var server; 
 
     beforeEach(function (done) {
-        var main = require('../../lib/server');
+        var main;
+
+        mockery.enable({
+            useCleanCache: true,
+            warnOnUnregistered: false
+        });
+
+        main = require('../../lib/server');
         server = main(done);
     });
 
     afterEach(function (done) {
-        server.stop(done);
+        server.stop(function () {
+            mockery.disable();
+            done();
+        });
     });
 
     it('returns the status', function (done) {
@@ -23,6 +34,22 @@ describe('Status Route', function () {
 
             expect(data).to.deep.equal({
                 status: 'OK'
+            });
+            done();
+        });
+    });
+
+    it('returns the status of everything', function (done) {
+        server.inject({
+            method: 'GET',
+            url: '/games/status/all'
+        }, function (response) {
+            var data;
+            expect(response.statusCode).to.equal(200);
+            data = JSON.parse(response.payload);
+
+            expect(data).to.deep.equal({
+                api: 'OK'
             });
             done();
         });
